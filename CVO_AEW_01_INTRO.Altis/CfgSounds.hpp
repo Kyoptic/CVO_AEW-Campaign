5ae91c74396d7f713c2fc4d597ce6225c75c7a24
Kyoptic <kyoptica3@gmail.com>
2025-06-14 15:57:34 +0100

    Airport Audio
    1) Announcement as helicopter departs
    2) Ambient audio that needs to be on a loop while players at airport.
    
    3) Updated to CfgSounds.hpp

--- CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
+++ CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
@@ -5,4 +5,14 @@ class CfgSounds {
         sound[] = {"data\riot.ogg", db+30.0,1.0};
         titles[] = {};
     };
+	class airport_announcement {
+        name = "riot";
+        sound[] = {"data\audio\airportannouncement.ogg", db+30.0,1.0};
+        titles[] = {};
+    };
+		class airport_crowd {
+        name = "riot";
+        sound[] = {"data\audio\airportcrowd.ogg", db+30.0,1.0};
+        titles[] = {};
+    };
 };
