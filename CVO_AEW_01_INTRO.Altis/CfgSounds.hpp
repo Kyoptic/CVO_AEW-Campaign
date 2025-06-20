677c7e99cff0f5338230094e9fa97110b89a1a31
OverlordZorn <dennisAzorn@gmail.com>
2025-06-20 20:31:01 +0200

    Update CfgSounds.hpp

--- CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
+++ CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
@@ -3,7 +3,7 @@ class CfgSounds {
     class riot {
         name = "riot";
         //sound[] = {"data\audio\riot.ogg", db+30.0,1.0};
-		sound[] = {"data\audio\riot.ogg", 5,1.0};
+		sound[] = {"data\audio\riot.ogg", 5,0.8};
         titles[] = {};
     };
     class riot_faded {
