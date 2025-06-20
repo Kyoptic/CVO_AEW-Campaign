264062625be5fcca4bcdc88360c4c015d7950f99
Kyoptic <kyoptica3@gmail.com>
2025-06-20 18:15:34 +0100

    Various Tweaks & Fixes to mission.sqm

--- CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
+++ CVO_AEW_01_INTRO.Altis/CfgSounds.hpp
@@ -2,7 +2,8 @@ class CfgSounds {
     sounds[] = {};
     class riot {
         name = "riot";
-        sound[] = {"data\audio\riot.ogg", db+30.0,1.0};
+        //sound[] = {"data\audio\riot.ogg", db+30.0,1.0};
+		sound[] = {"data\audio\riot.ogg", 5,1.0};
         titles[] = {};
     };
 	class airport_announcement {
