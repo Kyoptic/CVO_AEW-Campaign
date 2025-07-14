660f456247d68158eba42ee652f8d258c8e41280
OverlordZorn <dennisAzorn@gmail.com>
2025-07-14 22:40:40 +0200

    init

---
+++ CVO_AEW_03.Altis/cvo_defines/cvo_csc_destinations.hpp
@@ -0,0 +1,28 @@
+/*
+*   CVO Custom Supply Crates Define
+*
+*   TBA
+*
+*/
+
+class cvo_csc_destinations {
+    import base_infrontPlayer from cvo_csc_destinations;
+    import base_mapClick from cvo_csc_destinations;
+
+    class AEW_tarp: base_infrontPlayer {
+        displayName = "On the black tarp";   // Just as an example
+        description = "On the black tarp";
+
+        scope = 2;
+
+        class parameters {
+            mode = "OFFSET"; // "BEHIND", "OFFSET"
+            offset[] = { 0, 0, 1.5 };   // only used by OFFSET
+            reference = "cvo_csc_tarp"; // TARGET, PLAYER or missionNamespace variablename
+        };
+    };
+
+    class AEW_mapClick: base_mapClick {
+        scope = 2;
+    };
+};
